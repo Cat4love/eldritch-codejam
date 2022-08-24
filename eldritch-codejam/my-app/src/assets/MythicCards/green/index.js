b4bc8923bfddbf82e70ced87edcb2b2bdd89f87f
Andrei Plavinski <plavinskiandrei@gmail.com>
2022-08-24 10:11:32 +0300

    fix: fix image links

--- eldritch-codejam/my-app/src/assets/MythicCards/green/index.js
+++ eldritch-codejam/my-app/src/assets/MythicCards/green/index.js
@@ -1,41 +1,21 @@
-import green1 from './green1.png';
-import green2 from './green2.png';
-import green3 from './green3.png';
-import green4 from './green4.png';
-import green5 from './green5.png';
-import green6 from './green6.png';
-import green7 from './green7.png';
-import green8 from './green8.png';
-import green9 from './green9.png';
-import green10 from './green10.png';
-import green11 from './green11.png';
-import green12 from './green12.png';
-import green13 from './green13.png';
-import green14 from './green14.png';
-import green15 from './green15.png';
-import green16 from './green16.png';
-import green17 from './green17.png';
-import green18 from './green18.png';
-
 const cards = {
-  green1,
-  green2,
-  green3,
-  green4,
-  green5,
-  green6,
-  green7,
-  green8,
-  green9,
-  green10,
-  green11,
-  green12,
-  green13,
-  green14,
-  green15,
-  green16,
-  green17,
-  green18,
+  green1 : 'url(./assets/MythicCards/blue/green1.png)',
+  green2 : 'url(./assets/MythicCards/blue/green2.png)',
+  green3 : 'url(./assets/MythicCards/blue/green3.png)',
+  green4 : 'url(./assets/MythicCards/blue/green4.png)',
+  green5 : 'url(./assets/MythicCards/blue/green5.png)',
+  green6 : 'url(./assets/MythicCards/blue/green6.png)',
+  green7 : 'url(./assets/MythicCards/blue/green7.png)',
+  green8 : 'url(./assets/MythicCards/blue/green8.png)',
+  green9 : 'url(./assets/MythicCards/blue/green9.png)',
+  green10 : 'url(./assets/MythicCards/blue/green10.png)',
+  green11 : 'url(./assets/MythicCards/blue/green11.png)',
+  green12 : 'url(./assets/MythicCards/blue/green12.png)',
+  green13 : 'url(./assets/MythicCards/blue/green13.png)',
+  green14 : 'url(./assets/MythicCards/blue/green14.png)',
+  green15 : 'url(./assets/MythicCards/blue/green15.png)',
+  green16 : 'url(./assets/MythicCards/blue/green16.png)',
+  green17 : 'url(./assets/MythicCards/blue/green17.png)',
+  green18 : 'url(./assets/MythicCards/blue/green18.png)',
 };
-
 export default cards;
